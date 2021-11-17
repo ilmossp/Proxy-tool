0d218863e9a0f57504b8eea2670fb5d7a24b28fb
ILMOSS <medilyasselmoudene@gmail.com>
2021-11-17 13:14:42 +0100

    added custom input

--- proxy-tool.sh
+++ proxy-tool.sh
@@ -1,4 +1,12 @@
 ##test-comment:
+#read user input
+
+echo "input proxy address: "
+read user_proxy_address
+echo "input proxy port: "
+read user_proxy_port
+
+#use input in script
 cd /etc
 if [ -f environment.bak ]; 
 then
@@ -15,14 +23,14 @@ then
 else 
     echo "Enabling Proxy for the first time"
     sudo cp environment environment.bak 
-    sudo echo -e "http_proxy=\"http://10.23.201.11:3128/\"\n\
-https_proxy=\"http://10.23.201.11:3128/\"\n\
-ftp_proxy=\"http://10.23.201.11:3128/\"\n\
-rsync_proxy=\"http://10.23.201.11:3128/\"\n\
+    sudo echo -e "http_proxy=\"http://$user_proxy_address:$user_proxy_port/\"\n\
+https_proxy=\"http://$user_proxy_address:$user_proxy_port/\"\n\
+ftp_proxy=\"http://$user_proxy_address:$user_proxy_port/\"\n\
+rsync_proxy=\"http://$user_proxy_address:$user_proxy_port/\"\n\
 no_proxy=\"localhost,127.0.0.1,192.168.1.1,::1,*.local\"\n\
-HTTP_PROXY=\"http://10.23.201.11:3128/\"\n\
-HTTPS_PROXY=\"http://10.23.201.11:3128/\"\n\
-FTP_PROXY=\"http://10.23.201.11:3128/\"\n\
-RSYNC_PROXY=\"http://10.23.201.11:3128/\"\n\
+HTTP_PROXY=\"http://$user_proxy_address:$user_proxy_port/\"\n\
+HTTPS_PROXY=\"http://$user_proxy_address:$user_proxy_port/\"\n\
+FTP_PROXY=\"http://$user_proxy_address:$user_proxy_port/\"\n\
+RSYNC_PROXY=\"http://$user_proxy_address:$user_proxy_port/\"\n\
 NO_PROXY=\"localhost,127.0.0.1,192.168.1.1,::1,*.local\"\n" >> environment
 fi  
